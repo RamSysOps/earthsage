385b2a64a048c4e692f66cef3e98312882c7dea7
RAMAMURTHY <ramamurthy@ypccollege.edu.my>
2024-10-23 19:40:32 +0800

    Update Dbhandler.class.php
    updated admin details

--- classes/Dbhandler.class.php
+++ classes/Dbhandler.class.php
@@ -13,9 +13,9 @@ class Dbhandler {
 
   private function connect() {
     // default XAMPP credentials 
-    $this->host = "127.0.0.1";
-    $this->user = "root";
-    $this->pass = "";
+    $this->host = "13.213.77.85";
+    $this->user = "phpmyadmin_user";
+    $this->pass = "root";
     $this->db = "ogtech";
 
     // connect to db
@@ -29,7 +29,7 @@ class Dbhandler {
 
   public function conn() {
     // connect to db
-    $this->conn = new mysqli("127.0.0.1", "root", "", "ogtech");
+    $this->conn = new mysqli("13.213.77.85", "phpmyadmin_user", "root", "ogtech");
     return $this->conn;
 
     /* check connection */
